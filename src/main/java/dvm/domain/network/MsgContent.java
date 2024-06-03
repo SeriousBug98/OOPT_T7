a4fe28c71d33fa2adc47297b5fee2aafd644f787
yeonju73 <108404405+yeonju73@users.noreply.github.com>
2024-06-03 15:11:28 +0900

    Update MsgContent.java
    MsgContent update

--- src/main/java/dvm/domain/network/MsgContent.java
+++ src/main/java/dvm/domain/network/MsgContent.java
@@ -1,10 +1,33 @@
 package dvm.domain.network;
 
 public class MsgContent {
-    private int itemCode;
-    private int itemNum;
-    private int dvmX;
-    private int dvmY;
-    private String authenticationCode;
-    private boolean availability;
+    public int item_code;
+    public int item_num;
+    public int dvmX;
+    public int dvmY;
+    public String cert_code;
+    public boolean availability;
+
+    public MsgContent(int itemCode, int itemNum) {
+        this.item_code = itemCode;
+        this.item_num = itemNum;
+    }
+
+    public MsgContent(int itemCode, int itemNum, int dvmX, int dvmY) {
+        this.item_code = itemCode;
+        this.item_num = itemNum;
+        this.dvmX = dvmX;
+        this.dvmY = dvmY;
+    }
+
+    public MsgContent(int itemCode, int itemNum, String authenticationCode) {
+        this.item_code = itemCode;
+        this.item_num = itemNum;
+        this.cert_code = authenticationCode;
+    }
+    public MsgContent(int itemCode, int itemNum, boolean availability) {
+        this.item_code = itemCode;
+        this.item_num = itemNum;
+        this.availability = availability;
+    }
 }
